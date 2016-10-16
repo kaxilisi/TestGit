d88e08f9327db198282685b1c48210803278c7d8
apple <apple@Youngc-2.local>
2016-10-17 02:48:54 +0800

    22

---
+++ Test/Test/View.swift
@@ -0,0 +1,21 @@
+//
+//  View.swift
+//  Test
+//
+//  Created by apple on 2016/10/17.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+import UIKit
+
+class View: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
