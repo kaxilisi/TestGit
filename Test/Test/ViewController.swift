2fb6df2cca6f13cd4b9b69d7ffa9b14b505f94d5
apple <apple@Youngc-2.local>
2017-01-17 04:18:51 +0800

    00

--- Test/Test/ViewController.swift
+++ Test/Test/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        mmmmmmmm
         // Do any additional setup after loading the view, typically from a nib.
     }
 
