e11388c2e329957e724566f5ac935fd975de5399
apple <apple@Youngc-2.local>
2017-01-17 15:34:48 +0800

    9

--- Test/Test/ViewController.swift
+++ Test/Test/ViewController.swift
@@ -12,11 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-<<<<<<< HEAD
-        mmmmmmmm
-=======
         view.ViewController.ba
->>>>>>> master
         // Do any additional setup after loading the view, typically from a nib.
     }
 
